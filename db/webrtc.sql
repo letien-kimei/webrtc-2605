72c4d12f938d3079e5da9ec636330b10b7aec52a
letien-kimei <tienlm@kimei.vn>
2022-06-09 21:50:24 +0700

    update

--- db/webrtc.sql
+++ db/webrtc.sql
@@ -2,10 +2,10 @@
 -- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3307
--- Generation Time: Jun 01, 2022 at 01:06 AM
--- Server version: 10.1.38-MariaDB
--- PHP Version: 5.6.40
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th6 09, 2022 lúc 04:49 PM
+-- Phiên bản máy phục vụ: 10.1.38-MariaDB
+-- Phiên bản PHP: 5.6.40
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,62 +19,85 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `webrtc`
+-- Cơ sở dữ liệu: `webrtc`
 --
-CREATE DATABASE IF NOT EXISTS `webrtc` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `webrtc`;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `dtb_rooms`
+-- Cấu trúc bảng cho bảng `dtb_alert`
 --
 
-DROP TABLE IF EXISTS `dtb_rooms`;
-CREATE TABLE `dtb_rooms` (
+CREATE TABLE `dtb_alert` (
   `id` int(10) UNSIGNED NOT NULL,
   `room_id` text NOT NULL,
-  `room_name` varchar(255) DEFAULT NULL
+  `request_user_id` int(11) NOT NULL,
+  `message` text,
+  `accept` tinyint(1) NOT NULL DEFAULT '0',
+  `cancel` tinyint(1) NOT NULL DEFAULT '0',
+  `type` varchar(255) NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `dtb_rooms`
+-- Đang đổ dữ liệu cho bảng `dtb_alert`
 --
 
-INSERT INTO `dtb_rooms` (`id`, `room_id`, `room_name`) VALUES
-(4, 'bb2075a0-b33c-43ac-9796-23117fa6046b', NULL);
+INSERT INTO `dtb_alert` (`id`, `room_id`, `request_user_id`, `message`, `accept`, `cancel`, `type`, `active`) VALUES
+(13, 'be704ba7-4432-4803-a837-71ebfeb76ff8', 9, 'Nguyễn Văn A muốn tham gia nhóm Phòng 2', 1, 0, 'REQUEST_JOIN_ROOM', 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `dtb_rooms_users`
+-- Cấu trúc bảng cho bảng `dtb_rooms`
 --
 
-DROP TABLE IF EXISTS `dtb_rooms_users`;
+CREATE TABLE `dtb_rooms` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `room_id` text NOT NULL,
+  `room_name` varchar(255) DEFAULT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Đang đổ dữ liệu cho bảng `dtb_rooms`
+--
+
+INSERT INTO `dtb_rooms` (`id`, `room_id`, `room_name`, `user_id`) VALUES
+(4, 'bb2075a0-b33c-43ac-9796-23117fa6046b', NULL, 0),
+(23, '273886c4-978b-478b-a08d-bc32fb474418', 'Phòng 1', 9),
+(24, 'be704ba7-4432-4803-a837-71ebfeb76ff8', 'Phòng 2', 10);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `dtb_rooms_users`
+--
+
 CREATE TABLE `dtb_rooms_users` (
   `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `user_id_request` int(11) NOT NULL,
   `user_id_receive` int(11) NOT NULL,
-  `room_id` text NOT NULL
+  `room_id` text NOT NULL,
+  `pending` tinyint(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `dtb_rooms_users`
+-- Đang đổ dữ liệu cho bảng `dtb_rooms_users`
 --
 
-INSERT INTO `dtb_rooms_users` (`id`, `user_id`, `user_id_request`, `user_id_receive`, `room_id`) VALUES
-(4, 9, 9, 10, 'bb2075a0-b33c-43ac-9796-23117fa6046b'),
-(5, 10, 10, 9, 'bb2075a0-b33c-43ac-9796-23117fa6046b'),
-(6, 11, 11, 9, 'bb2075a0-b33c-43ac-9796-23117fa6046b');
+INSERT INTO `dtb_rooms_users` (`id`, `user_id`, `user_id_request`, `user_id_receive`, `room_id`, `pending`) VALUES
+(4, 9, 9, 10, 'bb2075a0-b33c-43ac-9796-23117fa6046b', 0),
+(5, 10, 10, 9, 'bb2075a0-b33c-43ac-9796-23117fa6046b', 0),
+(14, 9, 0, 0, 'be704ba7-4432-4803-a837-71ebfeb76ff8', 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `dtb_users`
+-- Cấu trúc bảng cho bảng `dtb_users`
 --
 
-DROP TABLE IF EXISTS `dtb_users`;
 CREATE TABLE `dtb_users` (
   `id` int(10) UNSIGNED NOT NULL,
   `username` varchar(255) NOT NULL,
@@ -84,57 +107,70 @@ CREATE TABLE `dtb_users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `dtb_users`
+-- Đang đổ dữ liệu cho bảng `dtb_users`
 --
 
 INSERT INTO `dtb_users` (`id`, `username`, `password`, `fullname`, `peer_id`) VALUES
-(9, 'usm1', '123', 'Lê Minh Tiến', 'b7873f66-437b-47a1-90b4-ecea9cf9b4ef'),
-(10, 'usm2', '123', 'Nguyễn Văn A', 'e18658f6-be3f-4a4b-9dc8-bee29b334b87'),
-(11, 'usm3', '123', 'Nguyễn Văn B', 'a95da330-60b5-4a05-80bc-73212c2396bc');
+(9, 'usm1', '123', 'Nguyễn Văn A', 'b7873f66-437b-47a1-90b4-ecea9cf9b4ef'),
+(10, 'usm2', '123', 'Nguyễn Văn B', 'e18658f6-be3f-4a4b-9dc8-bee29b334b87'),
+(11, 'usm3', '123', 'Nguyễn Văn C', 'a95da330-60b5-4a05-80bc-73212c2396bc'),
+(12, 'usm4', '123', 'Nguyễn Văn D', 'e186334FV-be3f-4a4b-9dc8-bee29b248DFSFF');
 
 --
--- Indexes for dumped tables
+-- Chỉ mục cho các bảng đã đổ
 --
 
 --
--- Indexes for table `dtb_rooms`
+-- Chỉ mục cho bảng `dtb_alert`
+--
+ALTER TABLE `dtb_alert`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `dtb_rooms`
 --
 ALTER TABLE `dtb_rooms`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `dtb_rooms_users`
+-- Chỉ mục cho bảng `dtb_rooms_users`
 --
 ALTER TABLE `dtb_rooms_users`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `dtb_users`
+-- Chỉ mục cho bảng `dtb_users`
 --
 ALTER TABLE `dtb_users`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT cho các bảng đã đổ
 --
 
 --
--- AUTO_INCREMENT for table `dtb_rooms`
+-- AUTO_INCREMENT cho bảng `dtb_alert`
+--
+ALTER TABLE `dtb_alert`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT cho bảng `dtb_rooms`
 --
 ALTER TABLE `dtb_rooms`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
--- AUTO_INCREMENT for table `dtb_rooms_users`
+-- AUTO_INCREMENT cho bảng `dtb_rooms_users`
 --
 ALTER TABLE `dtb_rooms_users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
--- AUTO_INCREMENT for table `dtb_users`
+-- AUTO_INCREMENT cho bảng `dtb_users`
 --
 ALTER TABLE `dtb_users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
